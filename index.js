1e65929e218dffe28fff4c4b3993726c4cb50b61
SahilFruitwala <sahilfruitwala007@gmail.com>
2021-11-22 15:44:29 -0400

    Done with Basic Swagger API Docs

--- index.js
+++ index.js
@@ -4,14 +4,76 @@ const express = require("express");
 const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
 
+// fileupload
+const fileupload = require('express-fileupload')
+
 const app = express();
 
 const swaggerDocument = YAML.load("./swagger.yaml");
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(express.json());
+app.use(fileupload());
+
+let courses = [
+  {
+    id: "11",
+    name: "Learn ReactJS",
+    price: 299,
+  },
+  {
+    id: "12",
+    name: "Learn Angular",
+    price: 399,
+  },
+  {
+    id: "13",
+    name: "Learn ExpressJs",
+    price: 499,
+  },
+];
 
 app.get("/", (req, res) => {
   res.status(200).send("<h1>Hello from LCO</h1>");
+});
+
+app.get("/api/v1/lco", (req, res) => {
+  res.send("Hello from LCO Docs");
+});
+
+app.get("/api/v1/lcoobject", (req, res) => {
+  res.status(200).json(courses[0]);
+});
+
+app.get("/api/v1/lcoarray", (req, res) => {
+  res.status(200).json(courses);
+});
+
+app.get("/api/v1/courses/:id", (req, res) => {
+  const myCourse = courses.find((course) => course.id === req.params.id);
+  res.json(myCourse);
+});
+
+app.get("/api/v1/courses", (req, res) => {
+  const location = req.query.location;
+  const device = req.query.device;
+
+  res.send({ location, device });
+});
+
+app.post("/api/v1/courses", (req, res) => {
+  console.log(req.body);
+  courses.push(req.body);
+  res.send(true);
+});
+
+app.post("/api/v1/coursesupload", (req, res) => {
+  const file = req.files.file
+  const path = __dirname + '/images/' + Date.now() + file.name
+
+  file.mv(path, (err) => {
+    res.send(true)
+  })
 });
 
 app.listen(4000, () => {
